1e034d194cfca4cd300174a50ad3bf8f5ef12fdc
masa95814 <masa95814@gmail.com>
2021-01-11 05:45:54 +0900

    Create develop branch

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase';
-import ENV from './env.json';
+// import ENV from './env.json';
 
-const config = {
-  apiKey:            ENV.FIREBASE_API_KEY,
-  authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
-  databaseURL:       ENV.FIREBASE_DB_URL,
-  projectId:         ENV.FIREBASE_PRJ_ID,
-  storageBucket:     ENV.FIREBASE_STRAGE,
-  messagingSenderId: ENV.FIREBASE_SENDER_ID,
-  appId: ENV.FIREBASE_APP_ID,
-  measurementId: ENV.FIREBASE_MEASUREMENT_ID,
-};
+// const config = {
+//   apiKey:            ENV.FIREBASE_API_KEY,
+//   authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
+//   databaseURL:       ENV.FIREBASE_DB_URL,
+//   projectId:         ENV.FIREBASE_PRJ_ID,
+//   storageBucket:     ENV.FIREBASE_STRAGE,
+//   messagingSenderId: ENV.FIREBASE_SENDER_ID,
+//   appId: ENV.FIREBASE_APP_ID,
+//   measurementId: ENV.FIREBASE_MEASUREMENT_ID,
+// };
 
 // Initialize Firebase
-firebase.initializeApp(config);
-firebase.analytics();
+// firebase.initializeApp(config);
+// firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
