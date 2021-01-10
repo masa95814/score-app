37b70356663b935a55197a143e9d547ed3fdb5ec
masa95814 <masa95814@gmail.com>
2021-01-11 02:58:15 +0900

    Setting firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC37kOfKYcmMtQ1_y4CeUS-cvrT0M0B2lI",
+  authDomain: "scoreapp-444f4.firebaseapp.com",
+  databaseURL: "https://scoreapp-444f4.firebaseio.com",
+  projectId: "scoreapp-444f4",
+  storageBucket: "scoreapp-444f4.appspot.com",
+  messagingSenderId: "197374940798",
+  appId: "1:197374940798:web:b7ae984d05a901ea996a2f",
+  measurementId: "G-XJ6YPG1CNC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
